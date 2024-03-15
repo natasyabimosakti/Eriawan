61bbe15c36e02466525a240719da1ee2e0dcc45f
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-03-15 23:48:34 +0700

    Update Eriawan4.js

--- ERI 1/Eriawan4.js
+++ ERI 1/Eriawan4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ERI 4
 // @namespace    http://tampermonkey.net/
-// @version      3.63
+// @version      3.64
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ERI%201/Eriawan4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ERI%201/Eriawan4.js
@@ -23,7 +23,7 @@ var refresh = 50;
 var namagroup1 = 'SHIOKELINCI';
 var Comment1 = '#shiokelinci4d*NANA19*28*46'; 
 
-var namagroup2 = 'PREDIKTOR';
+var namagroup2 = 'PREDIKSI HOKONG';
 var Comment2 = '#IYATOTO NAINDRA18 65*33*21'; 
 
 var namagroup3 = 'TiktakTogel';
