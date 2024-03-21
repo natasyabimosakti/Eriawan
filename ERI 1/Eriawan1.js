d5371a2fbc15c4b83e8439c0e2aaa10d3434ddbf
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-03-21 14:27:19 +0700

    Update Eriawan1.js

--- ERI 1/Eriawan1.js
+++ ERI 1/Eriawan1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ERI 1
 // @namespace    http://tampermonkey.net/
-// @version      3.73
+// @version      3.74
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ERI%201/Eriawan1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ERI%201/Eriawan1.js
