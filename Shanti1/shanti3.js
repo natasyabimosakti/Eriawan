35306b52e8693ab69e8a2539a386f4ae0c5c6e9a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-04-14 23:12:17 +0700

    Update shanti3.js

--- Shanti1/shanti3.js
+++ Shanti1/shanti3.js
@@ -117,7 +117,7 @@ var Comment32 = 'TESTBENTO';
 var jam = ["Baru sa","1 menit","2 menit","3 menit","4 menit","5 menit","6 menit","7 menit","8 menit","1 mnt","2 mnt","3 mnt","4 mnt","5 mnt","6 mnt","7 mnt","8 mnt"];
 
 
-var keyword = ["ROOM","R00M","R0M","R0OM","RO0M", "𝗥𝗢𝗢𝗠", "LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","SENIN","SELASA","RABU","KAMIS","JUMAT","JUMAAT","SABTU","MINGGU","LIMBA","ROM","🎱","⏩"];
+var keyword = ["ROOM","R00M","R0M","R0OM","RO0M", "𝗥𝗢𝗢𝗠", "SIDNEY","SINGAPORE","HONGKONG","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","SENIN","SELASA","RABU","KAMIS","JUMAT","JUMAAT","SABTU","MINGGU","LIMBA","ROM","🎱","⏩"];
 var Backlist1 = "GOJEK";
 var Backlist2 = "REKAP";
 var Backlist3 = "HASIL";
