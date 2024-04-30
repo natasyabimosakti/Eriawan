43e80a6b365239be69404297d01f2ea7019e06a8
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-04-30 13:09:16 +0700

    Update shanti4.js

--- Shanti1/shanti4.js
+++ Shanti1/shanti4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SHANTI 4
 // @namespace    http://tampermonkey.net/
-// @version      3.24
+// @version      3.25
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/Shanti1/shanti4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/Shanti1/shanti4.js
@@ -16,7 +16,7 @@
 var refresh = 50;
 
 var namagroup1 = 'TAFSIR';
-var Comment1 = 'TAG4D (HIWER313) 00*99*37'; 
+var Comment1 = 'TAG4D : (HIWER313) 00*99*37'; 
 
 var namagroup2 = 'K86T0T0';
 var Comment2 = 'K86TOTO ( HUJAN749 ) : 50*38*94'; 
@@ -42,7 +42,7 @@ var Comment8 = '#KITAJITU / HAIBER757 / 83*04*44 / #Betting';
 var namagroup9 = 'KIUTOTO';
 var Comment9 = '#(KIUTOTO) (HANTAR999) : 83*04*44';
 
-var namagroup10 = 'SL0TOG3L';
+var namagroup10 = 'SL0T0G3L';
 var Comment10 = '#SLOTOGEL (HIWER313) : 49*11*92'; 
 
 var namagroup11 = 'KEBAYA';
