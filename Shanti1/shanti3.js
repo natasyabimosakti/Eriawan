9f8c38117e1e26e7ee871a63c86e6fc69e40c8e6
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-05-02 23:48:54 +0700

    Update shanti3.js

--- Shanti1/shanti3.js
+++ Shanti1/shanti3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SHANTI 3
 // @namespace    http://tampermonkey.net/
-// @version      3.28
+// @version      3.29
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/Shanti1/shanti3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/Shanti1/shanti3.js
@@ -18,7 +18,7 @@ var refresh = 50;
 var namagroup1 = 'TAFSIR';
 var Comment1 = 'TAG4D : (ILOVE96) 17*40*16'; 
 
-var namagroup2 = 'K86T0T0';
+var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( INJURI45 ) : 17*40*16'; 
 
 var namagroup3 = 'INDOSL0TER';
