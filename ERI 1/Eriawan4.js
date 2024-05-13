af256a41a724234dc41c25833d591d3990742356
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-05-13 21:25:36 +0700

    Update Eriawan4.js

--- ERI 1/Eriawan4.js
+++ ERI 1/Eriawan4.js
@@ -122,7 +122,7 @@ var Comment32 = 'TESTBENTO';
 var jam = ["Baru sa","1 menit","2 menit","3 menit","4 menit","5 menit","6 menit","7 menit","8 menit","1 mnt","2 mnt","3 mnt","4 mnt","5 mnt","6 mnt","7 mnt","8 mnt"];
 
 
-var keyword = ["ROOM","R00M","R0M","R0OM","RO0M","𝗥𝗢𝗢𝗠","🎱","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","⏩","⬛"];
+var keyword = ["ROOM","R00M","R0M","R0OM","RO0M","𝗥𝗢𝗢𝗠","🎱","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","⏩","⬛","🏆"];
 
 var Backlist1 = "PREDIKSI";
 var Backlist2 = "REKAP";
