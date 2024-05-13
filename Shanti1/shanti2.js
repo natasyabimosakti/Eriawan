bd9c0b66abfbb3a114f8e7426c21f55861a7849c
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-05-13 19:14:52 +0700

    Update shanti2.js

--- Shanti1/shanti2.js
+++ Shanti1/shanti2.js
@@ -119,7 +119,7 @@ var jam = ["Baru sa","1 menit","2 menit","3 menit","4 menit","5 menit","6 menit"
 
 
 
-var keyword = ["ROOM","Room","R00M","R0M","R0OM","RO0M","𝗥𝗢𝗢𝗠","𝗥𝗢𝗢𝗠","🎱","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","⏩","█"];
+var keyword = ["ROOM","█","R00M","Room","𝗥𝗢𝗢𝗠","R0M","R0OM","RO0M","𝗥𝗢𝗢𝗠","🎱","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","⏩","BORG99"];
 
 var Backlist1 = "LIST";
 var Backlist2 = "REKAP";
