5a703677df5766a2b113d364d3decaa9e2293adc
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-04 00:27:20 +0700

    Update Shanti1.js

--- Shanti2/Shanti1.js
+++ Shanti2/Shanti1.js
@@ -15,8 +15,8 @@
 // ==/UserScript==
 var refresh = 50;
 
-var namagroup1 = 'TEBAK';
-var Comment1 = 'TOYA (OPPA31) 66*08*20';
+var namagroup1 = 'RITOGEL';
+var Comment1 = '#RITOGEL(OPPA31)66*08*20';
 
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( OYON22 ) : 21*84*64'; 
@@ -115,7 +115,7 @@ var Comment32 = 'TESTBENTO';
 
 var jam = ["Baru sa","1 menit","2 menit","3 menit","4 menit","5 menit","6 menit","7 menit","8 menit","1 mnt","2 mnt","3 mnt","4 mnt","5 mnt","6 mnt","7 mnt","8 mnt"];
 
-var keyword = ["ROOM","*ROOM","R00M L0MB4","█","R00M","Room","𝗥𝗢𝗢𝗠","R0M","R0OM","RO0M","𝗥𝗢𝗢𝗠","🎱","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","⏩","٠•●","BORG99","🟠"];
+var keyword = ["ROOM","*ROOM","L0MB4","█","R00M","Room","𝗥𝗢𝗢𝗠","R0M","R0OM","RO0M","𝗥𝗢𝗢𝗠","🎱","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","⏩","٠•●","BORG99","🟠","●•٠·"];
 
 
 var Backlist1 = "LIST";
