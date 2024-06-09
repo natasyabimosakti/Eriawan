ed9ef6a2a555ebc6374ced64f894c6dd3e2057cb
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-09 18:40:21 +0700

    Update eriawan1.js

--- ERI 2/eriawan1.js
+++ ERI 2/eriawan1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ERI 1
 // @namespace    http://tampermonkey.net/
-// @version      4.15
+// @version      4.16
 // @description  try to take over the world!
 // @updateURL https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ERI%202/eriawan1.js
 // @downloadURL https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ERI%202/eriawan1.js
@@ -123,7 +123,7 @@ var Comment32 = 'TESTBENTO';
 var jam = ["Baru sa","1 menit","2 menit","3 menit","4 menit","5 menit","6 menit","7 menit","8 menit","1 mnt","2 mnt","3 mnt","4 mnt","5 mnt","6 mnt","7 mnt","8 mnt"];
 
 
-var keyword = ["𝗥𝗢𝗢𝗠","ROOM_L0MBA","ROOM","R00M","R0M","R0OM","RO0M","🎱","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","⏩","⬛","🏆"];
+var keyword = ["𝗥𝗢𝗢𝗠","👑ROOM","ROOM_L0MBA","ROOM","R00M","R0M","R0OM","RO0M","🎱","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","⏩","⬛","🏆","👑"];
 
 var Backlist1 = "PREDIKSI";
 var Backlist2 = "REKAP";
