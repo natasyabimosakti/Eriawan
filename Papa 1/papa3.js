2d948dd3dd8e7836d632545935be28151a3c6000
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-14 04:49:48 +0700

    Update papa3.js

--- Papa 1/papa3.js
+++ Papa 1/papa3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PAPA 3
 // @namespace    http://tampermonkey.net/
-// @version      1.03
+// @version      1.04
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/Papa%201/papa3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/Papa%201/papa3.js
