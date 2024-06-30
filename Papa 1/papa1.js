8a4f5d2c5ab15f71ece986a4147a38eebf54b85b
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-30 14:57:57 +0700

    Update papa1.js

--- Papa 1/papa1.js
+++ Papa 1/papa1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PAPA 1
 // @namespace    http://tampermonkey.net/
-// @version      1.12
+// @version      1.13
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/Papa%201/papa1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/Papa%201/papa1.js
@@ -432,7 +432,15 @@ var myInterval = setInterval(function(){
                                 console.log("Sudah Comment")
                                 return;
                             }
-
+                                if (ceknamagroup.includes(namagroup17) == true||ceknamagroup1.includes(namagroup17) == true||ceknamagroup2.includes(namagroup17) == true||ceknamagroup3.includes(namagroup17) == true||ceknamagroup4.includes(namagroup17) == true) {
+                                /*cek nama group dan tulis commntar*/
+                                document.getElementsByClassName("multi-line-floating-textbox")[0].value = Comment17;
+                                clicksend();
+                                clearInterval(myInterval);
+                                clearInterval(myrefresh);
+                                console.log("Sudah Comment")
+                                return;
+                            }
 
 
                             return;
