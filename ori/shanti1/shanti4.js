e21f78cc89ca5f8b3a12234134330134c3ba095f
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-15 19:56:35 +0700

    Update shanti4.js

--- ori/shanti1/shanti4.js
+++ ori/shanti1/shanti4.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         SHANTI4
+// @name         polos SHANTI4
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
