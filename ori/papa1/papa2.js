ec4e28ae8127e06370997f1acac77b0457e4621a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-16 22:45:35 +0700

    Update papa2.js

--- ori/papa1/papa2.js
+++ ori/papa1/papa2.js
@@ -3,8 +3,8 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @updateURL  https://github.com/natasyabimosakti/Eriawan/blob/main/ori/papa1/papa2.js
-// @downloadURL  https://github.com/natasyabimosakti/Eriawan/blob/main/ori/papa1/papa2.js
+// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa2.js
+// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa2.js
 // @author       You
 // @match        http*://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
