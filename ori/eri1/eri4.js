c72abf3a0223d46cee12878a02ec7a6560566bbb
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-18 06:59:35 +0700

    Update eri4.js

--- ori/eri1/eri4.js
+++ ori/eri1/eri4.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         POLOS ERI4
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.21
 // @description  try to take over the world!
-// @updateURL https://github.com/natasyabimosakti/Eriawan/blob/main/ori/eri1/eri4.js
-// @downloadURL https://github.com/natasyabimosakti/Eriawan/blob/main/ori/eri1/eri4.js
+// @updateURL     https://github.com/natasyabimosakti/Eriawan/blob/main/ori/eri1/eri4.js
+// @downloadURL     https://github.com/natasyabimosakti/Eriawan/blob/main/ori/eri1/eri4.js
 // @author       You
 // @match        http*://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -59,10 +59,10 @@ var Comment14 = '#DENTOTO 24*66*80 ( NAMIA31 )';
 var namagroup15 = 'HATRICK';
 var Comment15 = 'VESPATOGEL (NARITA74)  40*48*58'; 
 
-var namagroup16 = '';
+var namagroup16 = 'BELUM';
 var Comment16 = '';
 
-var namagroup17 = '';
+var namagroup17 = 'JAWAK';
 var Comment17 = '';
 
 var d = new Date();
@@ -87,21 +87,9 @@ var id15 = await GM.getValue(15);
 var id16 = await GM.getValue(16);
 var id17 = await GM.getValue(17);
 
+var refresh = 50;
 
-
-
-
-
-
-
-
-
-
-var refresh = 40;
-
-
-
-var admin = ["angeline","ayesha","andy","aurel","anya","artha","ajosultan87","aldi","adelia","abditogel","aditia","andri","ananda","adm","ayesha","audi","alde","aldo","amoy","anissa","alexs","bella","boboho","bastian","bobby","base","bonar","brian","bala","celsia","chika","chandra","cinta","calvin","celine","conny","dea","dika","dollar","dewa","dewi","dinda","dinanti","denis","diana","dave","desi","dina","echa","erika","elly","erwin","febrian","fiana","farah","farid","gretha","garda","ghifa","hefi","habib","hana","hartono","hanna","intanbarbar","icha","inisial","indra","intan","jengat","john","jhone","jihan","jengat","je pe","jesika","jovanka","jess","kiky","kayla","kemonmusuh","kayla","kakakemon","katty","kumala","key","kendri","kumbara","kembar","karina","kautsar","kapten","lina","lianda","larissa","leksa","larashati","lehman","lidya","lusiana","leon","laura","maes","maha","mad","manu","miranda","mesa","manu","mahendra","multi","mariana","minions","melati","mimi","megan","mayor","mong","megaways","mardia","nathaya","nadya","naura","neng","neman","nayra","nayla","nadila","nella","nasution","novi","oun","oscar","otong","priyan","primus","plat","prediksi","petir","rizal","resti","rano","rm","ratu","riko","ruto","richatun","ria","roy","rendy","robby","sintia","sanchez","san","sejitu","sanjaya","siska","safar","sakura","sintia","shela","slooter","supriati","suhaidatul","slooter","sabrina","syaqeena","sloter","sandiego","sabrina","salsabila","sutanti","sinta","tink","tag","tiara","tania","thonex","wndt","wiena","jhone","herfizah","vonny","wahid","ujen","wahzo","wak","wijaya","fahresa","viona","virna","puput","yoky","yohana","yasmine","zurro","wok","hoihai","hefi","jenifer","kotna"];
+var admin = ["alde","audi","arxidi","adm","ayesha","aditia","ananda","baru","Baru","bobby","bastian","camb","celsia","cinta","che","dika","dewi","dewa","dentoto","flanella","flan","grey","hefi","hoihai","iyatoto","icha","ikan","jne","kembar","kembara","kotna","keitogel","kumbara","lianda","laura","lina","mad","mahendra","multi","muhammad","mariana","melati","neman","nella","novi","novita","nasution","oscar","puput","priyan","primus","ria","rio","ratu","roy","rendy","sonia","serena","Siâo Ciâ","sinta","salsabila","sandiego","san","sanchez","sanjaya","siska","serena","sofia","safar","tatang","tink","tiktak","vin","wok","whane","winata","yoky","yohana"];
 
 
 var keyword1 = "ROOM"
