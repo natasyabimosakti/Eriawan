c9d126206f70f6173293e3975e1d0b45c010bfbc
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-23 17:08:04 +0700

    Update shanti4.js

--- ori/shanti2/shanti4.js
+++ ori/shanti2/shanti4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Plontos shanti22
 // @namespace    http://tampermonkey.net/
-// @version      3.02
+// @version      3.03
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti4.js
