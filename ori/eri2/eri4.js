e6bbe0f8dce7159d2c181b26af421658a46d5903
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-24 13:37:35 +0700

    Update eri4.js

--- ori/eri2/eri4.js
+++ ori/eri2/eri4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 2 ERI4
 // @namespace    http://tampermonkey.net/
-// @version      2.08
+// @version      2.09
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri4.js
@@ -41,7 +41,7 @@ var Comment8 = 'ANGKER4D=SOMERSET34=07*65*50';
 var namagroup9 = 'GILA4D';
 var Comment9 = 'GILA4D=SENADA17=07*65*50'; 
 
-var namagroup10 = 'PEJUANG';
+var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(SOPHIA38)*62*37*15'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
