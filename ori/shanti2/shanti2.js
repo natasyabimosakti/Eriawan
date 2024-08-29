c7097a3b08d89a402d8c8e8270823ffcff14bb20
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-30 06:32:26 +0700

    Update shanti2.js

--- ori/shanti2/shanti2.js
+++ ori/shanti2/shanti2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 2 SHANTI2
 // @namespace    http://tampermonkey.net/
-// @version      1.12
+// @version      1.13
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti2.js
@@ -62,8 +62,8 @@ var Comment15 = '#(MAINTOGEL365) (QONCCER88) : 65*26*24';
 var namagroup16 = 'UJI';
 var Comment16 = 'CITA4D*QUEEN710*88*85*30';
 
-var namagroup17 = 'CIVIC';
-var Comment17 = '#CIVICTOTO (QUALY710) = 59*50';
+var namagroup17 = 'LING';
+var Comment17 = 'LING77 (QONCCER88) 59*50';
 
 var namagroup18 = 'CCcxccccc';
 var Comment18 = '#kosong';
