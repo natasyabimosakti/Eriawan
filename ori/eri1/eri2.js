1d551aa233cc089be36517a8d782b96792197c1b
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-01 00:50:08 +0700

    Update eri2.js

--- ori/eri1/eri2.js
+++ ori/eri1/eri2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI ERI2
 // @namespace    http://tampermonkey.net/
-// @version      2.10
+// @version      2.11
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri1/eri2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri1/eri2.js
@@ -71,7 +71,7 @@ var Comment18 = '';
 
 
 
-var refresh = 60;
+var refresh = 70;
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
