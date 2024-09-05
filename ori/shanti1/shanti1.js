ec010ae5c557cd461c8522fa387c530d0277618c
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-05 17:55:17 +0700

    Update shanti1.js

--- ori/shanti1/shanti1.js
+++ ori/shanti1/shanti1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 1 SHANTI1
 // @namespace    http://tampermonkey.net/
-// @version      1.19
+// @version      1.20
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti1.js
@@ -73,7 +73,7 @@ var Comment18 = '#kosong';
 
 
 
-var refresh = 40;
+var refresh = 50;
 
 var d = new Date();
 var hour = d.getHours();
