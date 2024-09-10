96afbcb862658a3f0c6579ecb961fa530f68f254
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-10 23:08:08 +0700

    Update eri4.js

--- ori/eri2/eri4.js
+++ ori/eri2/eri4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 2 ERI4
 // @namespace    http://tampermonkey.net/
-// @version      2.16
+// @version      2.17
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri4.js
@@ -21,7 +21,7 @@ var namagroup2 = 'BUKU';
 var Comment2 = 'IYATOTO SUNRISE33 57*72*80'; 
 
 var namagroup3 = 'TIKTAKTOGEL';
-var Comment3 = '#Tiktaktogel / SUMMER84 / 62 , 37 , 15'; 
+var Comment3 = 'Tiktaktogel / SUMMER84 / 62 , 37 , 15'; 
 
 var namagroup4 = 'GAIB';
 var Comment4 = 'GAIB4D=SEMANGAT45=60*59*40'; 
