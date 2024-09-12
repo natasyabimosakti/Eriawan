e9bf08c720f10d3e7be8a9efa7c915a0c9fb7eb6
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-12 19:52:35 +0700

    Update shanti2.js

--- ori/shanti2/shanti2.js
+++ ori/shanti2/shanti2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 2 SHANTI2
 // @namespace    http://tampermonkey.net/
-// @version      1.19
+// @version      1.20
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti2.js
@@ -69,7 +69,7 @@ var namagroup18 = 'CCcxccccc';
 var Comment18 = '#kosong';
 
 
-var refresh = 60;
+var refresh = 50;
 
 var d = new Date();
 var hour = d.getHours();
@@ -593,3 +593,15 @@ function closer() {
 
 
 }
+var lasturlku
+var jar = setInterval(function(){
+
+    if(location.href.includes("group")){
+        lasturlku = location.href;
+       
+    }
+    if(location.href.length <= 30 ){
+        location.href = lasturlku
+    }
+
+},1000)
