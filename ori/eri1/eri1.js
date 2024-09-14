53f743e860c7dfebc029bbbfa115862ece14ca42
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-14 16:51:29 +0700

    Update eri1.js

--- ori/eri1/eri1.js
+++ ori/eri1/eri1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI ERI1
 // @namespace    http://tampermonkey.net/
-// @version      2.18
+// @version      2.19
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri1/eri1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri1/eri1.js
@@ -57,7 +57,7 @@ var namagroup14 = 'DENTOTO';
 var Comment14 = '#DENTOTO 20*18*75 ( PIANG212 )'; 
 
 var namagroup15 = 'POMPA4D';
-var Comment15 = '#POMPA4D*PIQUE020*63*51*29*';
+var Comment15 = '#POMPA4D*PIQUE020*63*51*';
 
 var namagroup16 = 'MALAM';
 var Comment16 = 'K'; 
@@ -68,7 +68,7 @@ var namagroup18 = 'SIANG';
 var Comment18 = ''; 
 
 
-var refresh = 60;
+var refresh = 50;
 
 var d = new Date();
 var hour = d.getHours();
