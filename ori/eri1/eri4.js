3164ad107f9f98f812b03a789f378c7734c09883
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-16 15:25:23 +0700

    Update eri4.js

--- ori/eri1/eri4.js
+++ ori/eri1/eri4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI ERI4
 // @namespace    http://tampermonkey.net/
-// @version      2.29
+// @version      2.30
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri1/eri4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri1/eri4.js
@@ -118,8 +118,8 @@ var keyword7 = "ROM"
 var keyword8 = "R00M"
 var keyword9 = "login"
 var keyword10 = "𝐑𝐎𝐎𝐌"
-var keyword11 = "SGP"
-var keyword12 = "HONGKONG"
+var keyword11 = "ROOM"
+var keyword12 = "ROOM"
 
 var Backlist1 = "pemenang lomba";
 var Backlist2 = "rekap";
