bc74f06b14984c8e8e59891faf9efc00ca7cfe73
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-16 15:26:09 +0700

    Update eri2.js

--- ori/eri2/eri2.js
+++ ori/eri2/eri2.js
@@ -118,8 +118,8 @@ var keyword7 = "ROM"
 var keyword8 = "R00M"
 var keyword9 = "login"
 var keyword10 = "𝐑𝐎𝐎𝐌"
-var keyword11 = "SGP"
-var keyword12 = "HONGKONG"
+var keyword11 = "ROOM"
+var keyword12 = "ROOM"
 
 var Backlist1 = "pemenang lomba";
 var Backlist2 = "rekap";
