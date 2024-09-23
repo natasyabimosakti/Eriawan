8ccb2c7c8ae38835d5582d93a7d2b65af5b57133
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-24 05:55:18 +0700

    Update shanti1.js

--- ori/shanti1/shanti1.js
+++ ori/shanti1/shanti1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 1 SHANTI1
 // @namespace    http://tampermonkey.net/
-// @version      3.90
+// @version      3.91
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti1.js
@@ -67,8 +67,8 @@ var Comment16 = 'CITA4D*PONTA670*32*71*52';
 var namagroup17 = 'LING';
 var Comment17 = 'LING77 (PIQUE020) 72*75';
 
-var namagroup18 = 'CCcxccccc';
-var Comment18 = '#kosong';
+var namagroup18 = 'VESPA7093L';
+var Comment18 = 'VESPATOGEL PIQUE020 06*97*33';
 
 var refresh = 60;
 
