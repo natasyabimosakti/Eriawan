72c8f487a388d957cfad19a3d8b264e7f55aee28
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-26 00:37:54 +0700

    Update shanti4.js

--- ori/shanti1/shanti4.js
+++ ori/shanti1/shanti4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 1 SHANTI4
 // @namespace    http://tampermonkey.net/
-// @version      1.30
+// @version      1.31
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti4.js
@@ -113,11 +113,11 @@ var admin = ["angeline","aprillia","aza","azura","ace","ajosultan87","amoy","ara
 "naumi","na","nada","nyamark","natalia",
 "ori","odgj","oscar","over","ouschan",
 "priqittyu","putra","pasaran","prissia","pey","paw","putri","pratama","prediksi","pram",
-"rm","ratu","rambo","renada","ramadansyah","recceh","resti","roh","riko","riyanti","reborn","rahmawati",
+"rm","ratu","rambo","renada","ramadansyah","recceh","resti","roh","riko","riyanti","reborn","rahmawati","rania",
 "sijitogel","sakura","sa","syaqeena","sydney","suhendra","sarah","simanjuntak","sutanti","supyati","saputra","surya","shinta","simaremare","sch","siregar","samuel","sanchez","sandiego",
 "togel","toto","tan",
 "vasquez","vina",
-"waslo","wulandari","wiena","wndtfny","wardani",
+"waslo","wulandari","wiena","wndtfny","wardani","wikasalim",
 "xabiru",
 "yuli","yosephine","yeti","yanto","yoky",
 "zhelonk","zurrotul"];
