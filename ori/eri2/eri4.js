a47a424ec7a5662903979baec2bfe55583a83c5d
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-07 18:44:15 +0700

    Update eri4.js

--- ori/eri2/eri4.js
+++ ori/eri2/eri4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 2 ERI4
 // @namespace    http://tampermonkey.net/
-// @version      2.35
+// @version      2.36
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri4.js
@@ -117,7 +117,7 @@ var admin = ["ajosultan87","abys","adm","angeline","arang","aza","anatasya","adi
 "uuch","ujang",
 "vargas",
 "xabiru",
-"yansah","yana","zefanyaa"];
+"yansah","yana"];
 
 
 var keyword1 = "ROOM"
