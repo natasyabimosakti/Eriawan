c6be4c5592b33d059d574b84a351fda60b0485fe
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-02 05:37:33 +0700

    Update eri1.js

--- ori/eri2/eri1.js
+++ ori/eri2/eri1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 2 ERI1
 // @namespace    http://tampermonkey.net/
-// @version      2.34
+// @version      2.35
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri1.js
@@ -96,7 +96,7 @@ var id16 = await GM.getValue(16);
 var id17 = await GM.getValue(17);
 var id18 = await GM.getValue(18);
 
-var admin = ["ajosultan87","abys","adm","angeline","arang","aza","anatasya","adinda","arum","ayesha","alicia","ani","azahra","anto","aruna","amore","angka","aleantika","alifa",
+var admin = ["ajosultan87","abys","adm","angeline","arang","aza","anatasya","adinda","arum","ayesha","alicia","ani","azahra","anto","aruna","amore","angka","aleantika","alifa","abangku",
 "bela","baran","bambang","bogard",
 "chandra","clone","camb","chantika","chikaku",
 "dika","delon",
@@ -104,12 +104,12 @@ var admin = ["ajosultan87","abys","adm","angeline","arang","aza","anatasya","adi
 "flanella","fu","faiq","flo","feronika",
 "gates","gita",
 "hefi","hartono",
-"jaguar","junior","jie","jewels","jess","je pe",
+"jaguarr","junior","jie","jewels","jess","je pe",
 "lawu","lowati",
-"kembar","kaphal","kumbara","kusnandar","kamado","khawn",
+"kembar","kaphal","kumbara","kusnandar","kamado","khawn","kertanegara",
 "lasmana","laaelaay","loetoe","liu","langit","luvika",
-"mang","melati","megaways","miira","meilani",
-"nella","neman","nyelot","naumi","nattacha",
+"mang","melati","megaways","miira","meilani","macau",
+"nella","neman","nyelot","naumi","nattacha","noor",
 "own","offcial",
 "punama","prediction","priqittyu",
 "ran","ria","rm","rizal","raangkuti",
@@ -119,7 +119,6 @@ var admin = ["ajosultan87","abys","adm","angeline","arang","aza","anatasya","adi
 "vargas",
 "xabiru",
 "yansah","yana"];
-
 
 var keyword1 = "ROOM"
 var keyword2 = "𝗥𝗢𝗢𝗠"
@@ -132,7 +131,7 @@ var keyword8 = "R00M"
 var keyword9 = "login"
 var keyword10 = "𝐑𝐎𝐎𝐌"
 var keyword11 = "ROOM"
-var keyword12 = "ROOM"
+var keyword12 = "ℝ𝕆𝕆𝕄"
 
 var Backlist1 = "pemenang lomba";
 var Backlist2 = "rekap";
