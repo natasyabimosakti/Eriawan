de8248c76a9222326732ae81d5ebcc3b147d9c43
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-03 21:41:37 +0700

    Update papa21.js

--- ori/papa2/papa21.js
+++ ori/papa2/papa21.js
@@ -96,7 +96,7 @@ var id18 = await GM.getValue(18);
 
 var admin = ["ajosultan87","abys","adianti","adm","angeline","arang","aza","anatasya","adinda","arum","ayesha","alicia","ani","azahra","anto","aruna","amore","angka","aleantika","alifa","abangku",
 "bela","baran","bambang","bogard",
-"chandra","clone","camb","chantika","chikaku",
+"chandra","clone","camb","chantika","chikaku","cintya",
 "dika","delon",
 "ethes","elok",
 "flanella","fu","faiq","flo","feronika",
@@ -116,7 +116,7 @@ var admin = ["ajosultan87","abys","adianti","adm","angeline","arang","aza","anat
 "uuch","ujang",
 "vargas",
 "xabiru",
-"yansah","yana","zhie"];
+"yansah","yana","yoonk","zhie"];
 
 
 var keyword1 = "ROOM"
