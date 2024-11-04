95a73b7ae6ab791fe7f9cbf13343f7129a1c6f4a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-04 11:28:24 +0700

    Update papa1.js

--- ori/papa1/papa1.js
+++ ori/papa1/papa1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI PAPA1
 // @namespace    http://tampermonkey.net/
-// @version      1.42
+// @version      1.43
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa1.js
@@ -57,7 +57,7 @@ var namagroup14 = '(WARTO)';
 var Comment14 = '#(WARUNGTOTO) = (YOUPUB33) 49*35*74';
 
 var namagroup15 = 'TASIKTOTO';
-var Comment15 = '##TASIKTOTO(ZUMBA78) = 72*68';
+var Comment15 = '#TASIKTOTO(ZUMBA78) = 72*68';
 
 var namagroup16 = 'UJI';
 var Comment16 = 'CITA4D*ZUMMO28*72*68*84';
