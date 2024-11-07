6993250137453eb5c4d13ec6780ca065ec22eb16
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-07 20:26:37 +0700

    Update papa22.js

--- ori/papa2/papa22.js
+++ ori/papa2/papa22.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BO1 PAPA22
 // @namespace    http://tampermonkey.net/
-// @version      1.07
+// @version      1.08
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa2/papa22.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa2/papa22.js
@@ -56,8 +56,8 @@ var Comment13 = '#TOYIBSLOT ( YAMYAM44 ) : 72*23*06';
 var namagroup14 = 'DENTOTO';
 var Comment14 = '#DENTOTO 72*23*06 ( YAHYYA33 )'; 
 
-var namagroup15 = 'JPMANIA';
-var Comment15 = '#JPMANIA*YOUARE05*32*38*42*';
+var namagroup15 = 'NEMO';
+var Comment15 = 'NEMO4D (YOUARE05) : 32*38*42';
 
 var namagroup16 = 'Hoho';
 var Comment16 = 'YUPPIE54 : 32*38*66 #HOHOTOGEL';
