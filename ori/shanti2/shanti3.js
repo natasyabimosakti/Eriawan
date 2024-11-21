1702f57071de2dd12623b78b1aaa78989e37f78e
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-21 12:07:55 +0700

    Update shanti3.js

--- ori/shanti2/shanti3.js
+++ ori/shanti2/shanti3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 2 SHANTI3
 // @namespace    http://tampermonkey.net/
-// @version      1.44
+// @version      1.45
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti3.js
@@ -131,9 +131,9 @@ var keyword7 = "ROM"
 var keyword8 = "R00M"
 var keyword9 = "login"
 var keyword10 = "𝐑𝐎𝐎𝐌"
-var keyword11 = "ROOM"
-var keyword12 = "ROOM"
-var keyword13 = "ROOM"
+var keyword11 = "R00M"
+var keyword12 = "SINGAPURA"
+var keyword13 = "HONGKONG"
 var keyword14 = "ℝ𝕆𝕆𝕄"
 
 var Backlist1 = "pemenang lomba";
