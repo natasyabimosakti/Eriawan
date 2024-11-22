77e76c71f841184f57504fbfb213cef4e7258a86
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-22 18:49:28 +0700

    Update eri1.js

--- ori/eri1/eri1.js
+++ ori/eri1/eri1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI ERI1
 // @namespace    http://tampermonkey.net/
-// @version      2.50
+// @version      2.51
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri1/eri1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri1/eri1.js
@@ -100,7 +100,7 @@ var admin = ["ajosultan87","aldy","abys","adianti","adm","angeline","arang","aza
 "ethes","elok",
 "flanella","fu","faiq","flo","feronika",
 "gates","gita","gratis",
-"hefi","hartono","haikal","intan",
+"hefi","hartono","haikal","hanabi","intan",
 "jaguarr","junior","jie","jewels","jess","je pe",
 "lawu","lowati","lubis","loren",
 "kembar","kaphal","kumbara","kusnandar","kamado","khawn","kertanegara","kei",
