1f11a2e76917dc06095afb70535e3b5f9a69e949
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-22 11:13:12 +0700

    Update eri4.js

--- ori/eri1/eri4.js
+++ ori/eri1/eri4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI ERI4
 // @namespace    http://tampermonkey.net/
-// @version      2.49
+// @version      2.50
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri1/eri4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri1/eri4.js
@@ -101,7 +101,7 @@ var admin = ["ajosultan87","aldy","abys","adianti","adm","angeline","arang","aza
 "dika","delon",
 "ethes","elok",
 "flanella","fu","faiq","flo","feronika",
-"gates","gita",
+"gates","gita","gratis",
 "hefi","hartono","haikal","intan",
 "jaguarr","junior","jie","jewels","jess","je pe",
 "lawu","lowati","lubis","loren",
@@ -140,7 +140,7 @@ var Backlist3 = "hasil";
 var Backlist4 = "room lomba freebet";
 var Backlist5 = "prediksi";
 var Backlist6 = "result";
-var Backlist7 = "Juara";
+var Backlist7 = "juara";
 
 var myrefresh = setInterval(function(){
 
