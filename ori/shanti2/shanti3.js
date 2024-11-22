c24e210db9224e56a2c1e06a3c340da56f7bfdfc
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-22 18:12:06 +0700

    Update shanti3.js

--- ori/shanti2/shanti3.js
+++ ori/shanti2/shanti3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 2 SHANTI3
 // @namespace    http://tampermonkey.net/
-// @version      1.45
+// @version      1.46
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti3.js
