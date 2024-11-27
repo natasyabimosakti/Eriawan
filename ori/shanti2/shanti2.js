a6bb83c5d9af46232d222eeb99735556dafdf744
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-28 04:56:13 +0700

    Update shanti2.js

--- ori/shanti2/shanti2.js
+++ ori/shanti2/shanti2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 2 SHANTI2
 // @namespace    http://tampermonkey.net/
-// @version      1.49
+// @version      1.50
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti2.js
@@ -20,8 +20,8 @@ var Comment1 = 'VESPATOGEL (QONCCER88) 59*50*30';
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( QQCHERY88 ) : 65*26*24';
 
-var namagroup3 = 'SILA';
-var Comment3 = '‌#SILATOGEL*QIBAAT50*44*03*46*BETTING';
+var namagroup3 = 'KIKO';
+var Comment3 = '#KIKOTOTO (QIBAAT50) = 44*03';
 
 var namagroup4 = 'MAYAPADA';
 var Comment4 = 'BETT*Mayapada4D(BO)*QERJA99*63*34*87';
@@ -98,7 +98,7 @@ var id18 = await GM.getValue(18);
 var admin = ["angeline","aprillia","aza","ami","adianti","azura","ace","ajosultan87","aimi","arang","amoy","amzi","angka","amzii","arang","angka","adita","anggara","anggraini","adm","alicia","ayesha","adelya","aprove","angelica","agatha","aulia","amelia","andika","ayunda","artanti","artha","andinincy",
 "base","boboho","bogard","bmkg","bonar","barbar","bawah","bungaps","bella",
 "cila","casaundra","cindy","cantika","claudia","cucu","cintiya","chandra","chiyaa",
-"dina","dewi","deva","dodda","debby","daniell","dens","depan","dewi","dika",
+"dina","dewi","deva","dodda","debby","daniell","dens","depan","dewi","dika","done",
 "erlangga","elmina","erina","elisa","elok","eviana",
 "fischer","ferlita","fahresa","fais","febia","fatimm","fox",
 "gates","gandul","geboyy","gene","gita","ghefira",
