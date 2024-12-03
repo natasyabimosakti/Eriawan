a4574965aa0647225735cbdcab8894ff25d46176
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-03 08:28:19 +0700

    Update shanti1.js

--- ori/shanti1/shanti1.js
+++ ori/shanti1/shanti1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 1 SHANTI1
 // @namespace    http://tampermonkey.net/
-// @version      4.13
+// @version      4.14
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti1.js
@@ -40,7 +40,7 @@ var namagroup8 = 'TOK99T0T0';
 var Comment8 = 'Tok99Toto ( PUANCEN444 ) : 06*97*33';
 
 var namagroup9 = 'BLITAR4D';
-var Comment9 = '#BLITAR4D ( PUSING777 ) : 30*09*35';
+var Comment9 = '#BLITAR4D ( PUSING777 ) : 30*09';
 
 var namagroup10 = 'RNR303';
 var Comment10 = '#RNR303(POKEK909) : 67*81*64';
