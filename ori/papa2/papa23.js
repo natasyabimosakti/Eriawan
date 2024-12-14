0579cadea80d3a5c1dcb2cf59195061fcbaa1eb7
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-15 05:57:21 +0700

    Update papa23.js

--- ori/papa2/papa23.js
+++ ori/papa2/papa23.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BO1 PAPA23
 // @namespace    http://tampermonkey.net/
-// @version      1.08
+// @version      1.11
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa2/papa23.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa2/papa23.js
