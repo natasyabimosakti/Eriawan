b4285b2dec4b222c7a725468b7f6e15af2cd413d
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-15 17:02:43 +0700

    Update papa3.js

--- ori/papa1/papa3.js
+++ ori/papa1/papa3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI PAPA3
 // @namespace    http://tampermonkey.net/
-// @version      1.47
+// @version      1.48
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa3.js
@@ -15,7 +15,7 @@
 // ==/UserScript==
 
 var namagroup1 = 'BLITAR4D';
-var Comment1 = '#BLITAR4D ( XRAY0909 ) : 57*24*90';
+var Comment1 = '#BLITAR4D (XRAY0909) = 57*24';
 
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( XBAY875 ) : 52*43*04';
@@ -27,7 +27,7 @@ var namagroup4 = 'ISTANA';
 var Comment4 = 'JANGKAUTOTO(XLXL456) : 57*24*90';
 
 var namagroup5 = 'MAYAPADA';
-var Comment5 = 'BETT*Mayapada4D(BO)*XMIN111*52*43*04';
+var Comment5 = 'BETT*SingasariTOTO(BO)*XMIN111*52*43*04';
 
 var namagroup6 = 'DIVA4D';
 var Comment6 = '#DIVA4D (XEXEK90) = 02*13*07';
