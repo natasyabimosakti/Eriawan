a8cd935aaf3a11a3f92e2144a46225ace9289311
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-18 13:20:36 +0700

    Update papa1.js

--- ori/papa1/papa1.js
+++ ori/papa1/papa1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI PAPA1
 // @namespace    http://tampermonkey.net/
-// @version      1.51
+// @version      1.52
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa1.js
@@ -23,8 +23,8 @@ var Comment2 = 'K86TOTO ( ZOOLINE55 ) : 11*71*27';
 var namagroup3 = 'PUSAT LOMBA';
 var Comment3 = 'Shiotogel4d (ZAMBORE77) 14*50*71';
 
-var namagroup4 = 'ISTANA';
-var Comment4 = 'JANGKAUTOTO(ZIGZAG66) : 14*61*50';
+var namagroup4 = 'RIATOTO';
+var Comment4 = '#RIATOTO ZIGZAG66 = 14*61*50';
 
 var namagroup5 = 'MAYAPADA';
 var Comment5 = 'BETT*SingasariTOTO(BO)*ZEVVA34*26*31*67';
@@ -96,7 +96,7 @@ var id18 = await GM.getValue(18);
 var admin = ["angeline","aprillia","aza","ami","adianti","azura","ace","ajosultan87","aimi","arang","amoy","amzi","angka","amzii","arang","angka","adita","anggara","anggraini","adm","alicia","ayesha","adelya","aprove","angelica","agatha","aulia","amelia","andika","ayunda","artanti","artha","andinincy","asyati",
 "base","boboho","bogard","bmkg","bonar","barbar","bawah","bungaps","bella",
 "cila","cindy","cantika","claudia","cucu","cintiya","chandra","chiyaa","canz","chinostia",
-"dina","dewi","deva","dodda","debby","daniell","dens","depan","dewi","dika","done",
+"damara","dina","dewi","deva","dodda","debby","daniell","dens","depan","dewi","dika","done",
 "erlangga","elmina","erina","elisa","elok","eviana","eryna",
 "fischer","ferlita","fahresa","fais","febia","fatimm","fox",
 "gates","gandul","geboyy","gene","gita","ghefira",
@@ -112,7 +112,7 @@ var admin = ["angeline","aprillia","aza","ami","adianti","azura","ace","ajosulta
 "rm","ratu","rambo","renada","ramadansyah","recceh","resti","roh","riko","riyanti","reborn","rahmawati","rania",
 "sijitogel","sakura","syaqeena","sydney","suhendra","sarah","simanjuntak","sutanti","supyati","saputra","surya","shinta","simaremare","sch","siregar","samuel","sanchez","sandiego","scatter","sahla","shell","sundawa",
 "togel","toto","tan","ten",
-"vasquez","vina",
+"vasquez","vina","valencia",
 "waslo","wulandari","wiena","wndtfny","wardani","wikasalim","wijaya",
 "xabiru","105166432041525",
 "yuli","yosephine","yeti","yanto","yoky",
