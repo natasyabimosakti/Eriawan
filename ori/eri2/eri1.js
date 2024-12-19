d219e22d8194fb644660d23074e64701acd74cda
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-19 22:12:19 +0700

    Update eri1.js

--- ori/eri2/eri1.js
+++ ori/eri2/eri1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 2 ERI1
 // @namespace    http://tampermonkey.net/
-// @version      2.53
+// @version      2.54
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri1.js
@@ -29,8 +29,8 @@ var Comment4 = 'GAIB4D=TANGGO10=77*81*33';
 var namagroup5 = 'KEITOGEL';
 var Comment5 = '#keitogel = (TAHUTOK50) = 48*16*34'; 
 
-var namagroup6 = 'Pasar';
-var Comment6 = 'PAJAKTOTO(TIRAMISU37) : 14*76';
+var namagroup6 = 'PEDRO';
+var Comment6 = '#PEDRO4D (TIRAMISU37*14*76*70)';
 
 var namagroup7 = '453P VIP';
 var Comment7 = 'ASEPTOGEL TALKLESS100 14*76*70'; 
@@ -94,7 +94,7 @@ var id16 = await GM.getValue(16);
 var id17 = await GM.getValue(17);
 var id18 = await GM.getValue(18);
 
-var admin = ["ajosultan87","aldy","abys","adianti","adm","angeline","arang","aza","anatasya","adinda","arum","ayesha","alicia","ani","azahra","anto","aruna","amore","angka","aleantika","alifa","abangku","amirah",
+var admin = ["ajosultan87","aldy","azahra","abys","adianti","adm","angeline","arang","aza","anatasya","adinda","arum","ayesha","alicia","ani","azahra","anto","aruna","amore","angka","aleantika","alifa","abangku","amirah",
 "bela","baran","bambang","bogard",
 "chandra","clone","camb","chantika","cintya","casaundra",
 "dika","delon",
