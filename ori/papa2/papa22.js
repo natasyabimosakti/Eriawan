ace2923307621c1da711572ba64ce42e57588025
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-21 09:29:10 +0700

    Update papa22.js

--- ori/papa2/papa22.js
+++ ori/papa2/papa22.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BO1 PAPA22
 // @namespace    http://tampermonkey.net/
-// @version      1.14
+// @version      1.15
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa2/papa22.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa2/papa22.js
@@ -125,7 +125,7 @@ var keyword5 = "𝐋𝐎𝐌𝐁𝐀"
 var keyword6 = "LIMBA"
 var keyword7 = "ROM"
 var keyword8 = "R00M"
-var keyword9 = "login"
+var keyword9 = "ROom"
 var keyword10 = "𝐑𝐎𝐎𝐌"
 var keyword11 = "SINGAPORE"
 var keyword12 = "SINGAPURA"
