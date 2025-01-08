6cf092fab21fad20fcf5c6a58af5a72317b2bef2
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-01-08 12:11:09 +0700

    Update papa3.js

--- ori/papa1/papa3.js
+++ ori/papa1/papa3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI PAPA3
 // @namespace    http://tampermonkey.net/
-// @version      1.58
+// @version      1.60
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa3.js
@@ -139,7 +139,7 @@ var Backlist3 = "hasil";
 var Backlist4 = "room lomba freebet";
 var Backlist5 = "prediksi";
 var Backlist6 = "result";
-var Backlist7 = "result";
+var Backlist7 = "totomacau";
 
 var myrefresh = setInterval(function(){
     if (tm == "" || tm == undefined || tm == null) {
