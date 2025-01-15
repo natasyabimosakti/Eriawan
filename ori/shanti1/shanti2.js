dbd5c8ed80768aefb12bfbd6e0d72816f34cddda
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-01-15 11:35:56 +0700

    Update shanti2.js

--- ori/shanti1/shanti2.js
+++ ori/shanti1/shanti2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 1 SHANTI2
 // @namespace    http://tampermonkey.net/
-// @version      1.74
+// @version      1.75
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti2.js
@@ -119,7 +119,7 @@ var admin = ["angeline","aprillia","aza","ami","adianti","azura","ace","ajosulta
 "vasquez","vina","valencia",
 "waslo","wulandari","wiena","wndtfny","wardani","wikasalim","wijaya",
 "xabiru","105166432041525",
-"yuli","yosephine","yeti","yanto","yoky",
+"yuli","yosephine","yeti","yanto","yoky","yati",
 "zhelonk","zimbabwe","zhie","zurrotul"];
 
 
