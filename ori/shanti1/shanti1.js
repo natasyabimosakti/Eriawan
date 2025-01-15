92f29bb7e71196997912119eeab0aadfd4b3a0d0
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-01-15 11:35:33 +0700

    Update shanti1.js

--- ori/shanti1/shanti1.js
+++ ori/shanti1/shanti1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 1 SHANTI1
 // @namespace    http://tampermonkey.net/
-// @version      4.29
+// @version      4.30
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti1.js
@@ -115,7 +115,7 @@ var admin = ["angeline","aprillia","aza","ami","adianti","azura","ace","ajosulta
 "vasquez","vina","valencia",
 "waslo","wulandari","wiena","wndtfny","wardani","wikasalim","wijaya",
 "xabiru","105166432041525",
-"yuli","yosephine","yeti","yanto","yoky",
+"yuli","yosephine","yeti","yanto","yoky","yati",
 "zhelonk","zimbabwe","zhie","zurrotul"];
 
 var keyword1 = "ROOM"
