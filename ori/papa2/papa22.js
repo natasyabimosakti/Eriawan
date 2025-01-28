946f47f22d941109de5feb3ba94284a23b712c99
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-01-28 10:45:00 +0700

    Update papa22.js

--- ori/papa2/papa22.js
+++ ori/papa2/papa22.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BO1 PAPA22
 // @namespace    http://tampermonkey.net/
-// @version      1.19
+// @version      1.20
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa2/papa22.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa2/papa22.js
@@ -105,7 +105,7 @@ var admin = ["ajosultan87","azahra","aldy","abys","adianti","adm","angeline","ar
 "lawu","lowati","lubis","loren",
 "kembar","kaphal","kumbara","kusnandar","kamado","khawn","kertanegara","kei",
 "lasmana","laaelaay","loetoe","liu","langit","luvika","lalla",
-"mang","melati","megaways","miira","meilani","macau",
+"mang","melati","megaways","miira","meilani","macau","mbah",
 "nella","neman","nyelot","naumi","nattacha","noor",
 "own","offcial","ozawa",
 "punama","prediction","priqittyu","poetry",
