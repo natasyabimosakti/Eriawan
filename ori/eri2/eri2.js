bf8e66d374a46445b87adbfb4ac3eb9e27587b74
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-02-01 22:44:56 +0700

    Update eri2.js

--- ori/eri2/eri2.js
+++ ori/eri2/eri2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 2 ERI2
 // @namespace    http://tampermonkey.net/
-// @version      2.60
+// @version      2.61
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri2.js
@@ -106,7 +106,7 @@ var admin = ["ajosultan87","aldy","azahra","abys","adianti","adm","angeline","ar
 "kembar","kaphal","kumbara","kusnandar","kamado","khawn","kertanegara","kei",
 "lasmana","laaelaay","loetoe","liu","langit","luvika","lalla",
 "mang","melati","megaways","miira","meilani","macau","mbah",
-"nella","neman","nyelot","naumi","nattacha","noor",
+"nella","neman","nyelot","naumi","nattacha","noor","noah",
 "own","offcial","ozawa",
 "punama","prediction","priqittyu","poetry",
 "rm","rizal","raangkuti","rahman","rangga",
