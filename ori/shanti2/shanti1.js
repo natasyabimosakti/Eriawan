82bb01475a4cd6ebd9aca754c4cfaa8ec5d5122d
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-02-10 09:36:13 +0700

    Update shanti1.js

--- ori/shanti2/shanti1.js
+++ ori/shanti2/shanti1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 2 SHANTI1
 // @namespace    http://tampermonkey.net/
-// @version      1.78
+// @version      1.79
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti1.js
@@ -43,7 +43,7 @@ var namagroup9 = 'BLITAR4D';
 var Comment9 = '#BLITAR4D (TIKTOK2024) = 02*58';
 
 var namagroup10 = 'BENIHTOTO';
-var Comment10 = '#BENIHTOTO(THANKYOU99) : 90*97*78';
+var Comment10 = 'BENIHTOTO(THANKYOU99) : 90*97*78';
 
 var namagroup11 = 'DIVA4D';
 var Comment11 = '#DIVA4D (TUNAI123) = 02*58*15';
