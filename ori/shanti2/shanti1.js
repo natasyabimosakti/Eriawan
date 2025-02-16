6d5f8a37c486f0f26e42efab98154ecce9ca3707
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-02-16 16:23:20 +0700

    Update shanti1.js

--- ori/shanti2/shanti1.js
+++ ori/shanti2/shanti1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 2 SHANTI1
 // @namespace    http://tampermonkey.net/
-// @version      1.80
+// @version      1.81
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti1.js
@@ -39,8 +39,8 @@ var Comment7 = '#RIATOTO TAS999 = 37*25*47';
 var namagroup8 = '𝐕𝐈𝐏';
 var Comment8 = 'Tok99Toto ( TASTY89 ) : 37*25*47';
 
-var namagroup9 = 'BLITAR4D';
-var Comment9 = '#BLITAR4D (TIKTOK2024) = 02*58';
+var namagroup9 = 'Ultratogel88';
+var Comment9 = 'ULTRATOGEL88 = (TORANZ3) 02*58*15';
 
 var namagroup10 = 'BENIHTOTO';
 var Comment10 = 'BENIHTOTO(THANKYOU99) : 90*97*78';
@@ -90,9 +90,9 @@ var id15 = await GM.getValue(15);
 var id16 = await GM.getValue(16);
 var id17 = await GM.getValue(17);
 var id18 = await GM.getValue(18);
-var refresh = 50;
+var refresh = 40;
 var admin = ["angeline","ardiyanti","astrina","andinincy","asyati","alika","ayesha",
-             "adelya","ayunda","aurel","ardiansyah","angga","aldo","angelica","angka","aurora",
+             "adelya","ayunda","aurel","ardiansyah","angga","aldo","angelica","angka","aurora","amzi","arabelle",
              "boboho","bogard","boss","bungaps","bawah",
              "catherine","cristina","claura","cindy","chinostia","chandra",
              "damara","done","dani","dina","dewa","dewi","desi","deva","denis","depan","dika",
@@ -103,7 +103,7 @@ var admin = ["angeline","ardiyanti","astrina","andinincy","asyati","alika","ayes
              "irine","ishaura","ichaa","indah","iman","ikan","inem",
              "javeline","jovanka","junior","jasmine",
              "kimberly","kasino","kembar","kumbara",
-             "laura","lidya","leon","lomba",
+             "laura","lidya","leon","lomba","lopez",
              "mad","mas har","michelle","marimarshell","monicha","manu","mayangsari","marcia","megan","mirana",
              "new9eneration","nurlena","nasharu",
              "ozawa","otong","oscar","ouschan",
@@ -111,7 +111,7 @@ var admin = ["angeline","ardiyanti","astrina","andinincy","asyati","alika","ayes
              "queen",
              "riatoto","ruang","rani","rohmanu","rinx","rambo","rio",
              "sasa","sintia","syaqeena","susannah","sundawa","sanz","suzanawati","satria","setiawan",
-             "shell","sutanti","salsabila","saya","simaremare","sanchez","sydney","sandiego",
+             "shell","sutanti","salsabila","saya","simaremare","sanchez","sydney","sandiego","safitri",
              "toto","thonex",
              "vero",
              "wulan","wei",
