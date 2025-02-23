ba3d2c8abbb6d3c99ea594ccc8ee85fdf6f03ab9
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-02-23 20:20:13 +0700

    Update eri1.js

--- ori/eri2/eri1.js
+++ ori/eri2/eri1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 2 ERI1
 // @namespace    http://tampermonkey.net/
-// @version      2.67
+// @version      2.68
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri1.js
@@ -53,8 +53,8 @@ var Comment12 = 'TOGELKUY TERANG93 77*81*33';
 var namagroup13 = 'LICIN';
 var Comment13 = '#LICIN4D (TRUST898) 22*71*42';
 
-var namagroup14 = 'DENTOTO';
-var Comment14 = '#DENTOTO 30*13*87 ( TORNADO13 )'; 
+var namagroup14 = 'STREMING';
+var Comment14 = 'SAHARA888 ( TENTREM20 ) 30*13*87';
 
 var namagroup15 = 'Hoho';
 var Comment15 = 'TALKLESS100 : 83*08*25 #HOHOTOGEL';
@@ -108,7 +108,7 @@ var admin = ["andika","anto","angelina","aura","anisa","aprilia","ayesha","alici
              "nella","nadia",
              "putri","pedro","puput",
              "rangga","ria",
-             "sari","sinta","syaqeena","sandiego","sadewa","serena","kardova","sidebar","saya","sundawa",
+             "sari","sinta","syaqeena","sandiego","sadewa","serena","kardova","sidebar","saya","sundawa","sahara",
              "tink","tesa","toto",
              "vonny",
              "wijaya",
