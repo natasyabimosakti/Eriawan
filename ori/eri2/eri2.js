64efd5d4776ebcd5fa2179ca005d74b6f0c19916
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-02-23 20:21:30 +0700

    Update eri2.js

--- ori/eri2/eri2.js
+++ ori/eri2/eri2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 2 ERI2
 // @namespace    http://tampermonkey.net/
-// @version      2.67
+// @version      2.68
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri2.js
@@ -53,8 +53,8 @@ var Comment12 = 'TOGELKUY QURBAN05 82*68*61';
 var namagroup13 = 'LICIN';
 var Comment13 = '#LICIN4D (QERBAU04) 36*18*39';
 
-var namagroup14 = 'DENTOTO';
-var Comment14 = '#DENTOTO 58*96*00 ( QERJA99 )'; 
+var namagroup14 = 'STREMING';
+var Comment14 = 'SAHARA888 ( QUICKLY8 ) 58*96*00';
 
 var namagroup15 = 'Hoho';
 var Comment15 = 'QRISS08 : 06*04*01 #HOHOTOGEL';
@@ -109,7 +109,7 @@ var admin = ["andika","anto","angelina","aura","anisa","aprilia","ayesha","alici
              "nella","nadia",
              "putri","pedro","puput",
              "rangga","ria",
-             "sari","sinta","syaqeena","sandiego","sadewa","serena","kardova","sidebar","saya","sundawa",
+             "sari","sinta","syaqeena","sandiego","sadewa","serena","kardova","sidebar","saya","sundawa","sahara",
              "tink","tesa","toto",
              "vonny",
              "wijaya",
