a20aa6a1f54e0640a83bb5da1eea0e261f41fc50
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-03-23 00:03:23 +0700

    Update papa22.js

--- ori/papa2/papa22.js
+++ ori/papa2/papa22.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BO1 PAPA22
 // @namespace    http://tampermonkey.net/
-// @version      1.40
+// @version      1.41
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa2/papa22.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa2/papa22.js
@@ -44,8 +44,8 @@ var Comment9 = 'GILA4D=YUKWIN09=88*26*64';
 var namagroup10 = 'JNETOOTO';
 var Comment10 = '#JNETOTO(YUKJUM35)*88*26*64'; 
 
-var namagroup11 = 'Tukangtoto';
-var Comment11 = '( TUKANGTOTO YELIGUK88 : 63*47 )';
+var namagroup11 = 'PASAR';
+var Comment11 = 'DAGELAN4D(YELIGUK88) : 63*47*96';
 
 var namagroup12 = 'MASTER';
 var Comment12 = 'TOGELKUY YEYEK001 63*47*96'; 
@@ -95,28 +95,27 @@ var refresh = 40;
 
 var admin = ["andika","anto","angelina","aura","anisa","aprilia","ayesha","alicia","ajosultan87","aqisya","azahra","aini",
              "audi","andre","arem","alyn","alicia","asri","aldi","abys","angeline",
-             "ban nee","bella","biru","bela","baran",
+             "ban nee","bella","biru","bela","baran","bogard",
              "chandra","cengkiledu","chintya","clone","casaundra","chikaku","callista",
              "dika","dewa","dea","damara",
              "ethes","enzo",
-             "faiq","fania",
+             "faiq","fania","ferlita",
              "gita","gania","global","gambrong",
              "hefi","howel","hoi","hokage","haikal",
              "iyatoto","ikan","inem","ivan","ichaa","intan",
              "jesica","jne","jenifer","jessica","jovanca","jati",
              "kembar","keysha","kotna","kris bee","kolong",
-             "lalla","lenkova","loetoe","laura","luthfi","lomba","lupin",
+             "lalla","lenkova","loetoe","laura","luthfi","lomba","lupin","langit",
              "maharani","maley","mrdepowd","mad rm","miira","mata","manu",
              "nella","nadia",
              "putri","pedro","puput",
-             "rangga","rere",
+             "rangga","rere","rambo","ratuliu",
              "sari","sinta","syaqeena","sandiego","sadewa","serena","kardova","sidebar","saya","sundawa","sahara","sembroh",
              "tink","tesa","uuch","toto","utarii","tiar",
              "vonny",
              "wijaya","wawan",
              "xabiru",
              "yoky"];
-
 
 var keyword1 = "ROOM"
 var keyword2 = "𝗥𝗢𝗢𝗠"
