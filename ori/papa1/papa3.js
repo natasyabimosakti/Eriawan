31e81bdfd2dd7b689722a66e3f279a55b5ca5737
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-03-25 21:52:07 +0700

    Update papa3.js

--- ori/papa1/papa3.js
+++ ori/papa1/papa3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI PAPA3
 // @namespace    http://tampermonkey.net/
-// @version      1.76
+// @version      1.77
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa3.js
@@ -105,7 +105,7 @@ var admin = ["angeline","ardiyanti","astrina","andinincy","asyati","alika","ayes
              "javeline","jovanka","junior","jasmine","jati",
              "kimberly","kasino","kembar","kumbara",
              "laura","lidya","leon","lomba","lopez","lehman",
-             "mad","mas har","michelle","marimarshell","monicha","manu","mayangsari","marcia","megan","mirana",
+             "mad","mas har","michelle","marimarshell","monicha","manu","mayangsari","marcia","megan","mirana","monica",
              "new9eneration","nurlena","nasharu",
              "ozawa","otong","oscar","ouschan",
              "pencari","putri","pricilia","putra","putu",
