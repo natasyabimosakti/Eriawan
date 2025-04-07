f99ebe83f1910f37c8da5cb471170d6955742cb6
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-04-07 21:24:58 +0700

    Update papa4.js

--- ori/papa1/papa4.js
+++ ori/papa1/papa4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI PAPA4
 // @namespace    http://tampermonkey.net/
-// @version      1.72
+// @version      1.73
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa4.js
