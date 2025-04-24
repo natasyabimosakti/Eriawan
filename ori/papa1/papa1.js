76056f53e5b46012eff4066a279891cb7a507d34
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-04-24 18:07:57 +0700

    Update papa1.js

--- ori/papa1/papa1.js
+++ ori/papa1/papa1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI PAPA1
 // @namespace    http://tampermonkey.net/
-// @version      1.81
+// @version      1.82
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa1.js
@@ -131,7 +131,7 @@ var keyword8 = "R00M"
 var keyword9 = "R0M"
 var keyword10 = "𝐑𝐎𝐎𝐌"
 var keyword11 = "Ro0M"
-var keyword12 = "LOMBA"
+var keyword12 = "L0MB4"
 var keyword13 = "Room"
 var keyword14 = "ℝ𝕆𝕆𝕄"
 
