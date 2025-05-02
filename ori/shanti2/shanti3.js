6669b2da3ec808fc06757b7ea81af92972601b88
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-02 22:18:39 +0700

    Update shanti3.js

--- ori/shanti2/shanti3.js
+++ ori/shanti2/shanti3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 2 SHANTI3
 // @namespace    http://tampermonkey.net/
-// @version      1.89
+// @version      1.90
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti2/shanti3.js
@@ -29,8 +29,8 @@ var Comment4 = 'BETT*Mayapada4D(BO)*RORO363*76*45*27';
 var namagroup5 = 'TAFSIR';
 var Comment5 = 'SIJITOGEL RAMPOK84 61*35*98';
 
-var namagroup6 = 'LTDTOTO';
-var Comment6 = '#LTDTOTO =(REALIS147)= 18*75*';
+var namagroup6 = 'KASTOTO';
+var Comment6 = '#KASTOTO(REALIS147) = 18*75*17 #AGENTOGELTERPERCAYA';
 
 var namagroup7 = 'RIATOTO';
 var Comment7 = '#RIATOTO ROOM29 = 41*91*29';
@@ -95,7 +95,7 @@ var refresh = 40;
 var admin = ["angeline","ardiyanti","astrina","andinincy","asyati","alika","ayesha","azahra",
              "adelya","ayunda","aurel","ardiansyah","angga","aldo","angelica","angka","aurora","arabelle","anggel",
              "boboho","bogard","boss","bungaps","bawah","botack",
-             "catherine","cristina","claura","cindy","chinostia","chandra",
+             "catherine","cristina","claura","cindy","chinostia","chandra","cincai",
              "damara","done","dani","dina","dewa","dewi","desi","deva","denis","depan","dika","debby",
              "evelyn","erwin","elisa","eryna","echa",
              "fira","felicia","fillea",
