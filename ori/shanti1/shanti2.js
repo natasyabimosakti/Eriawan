dbb0dda35ea4f1c12edabcb8512b6fd65b084eeb
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-19 23:28:17 +0700

    Update shanti2.js

--- ori/shanti1/shanti2.js
+++ ori/shanti1/shanti2.js
@@ -94,7 +94,7 @@ var id16 = await GM.getValue(16);
 var id17 = await GM.getValue(17);
 var id18 = await GM.getValue(18);
 
-var refresh = 40;
+var refresh = 50;
 
 var admin = ["angeline","ardiyanti","astrina","andinincy","asyati","alika","ayesha","anggel","azahra",
              "adelya","ayunda","aurel","ardiansyah","angga","aldo","angelica","angka","aurora","arabelle",
