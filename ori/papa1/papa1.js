2ba7ca737759b2df2e73a524f2fc2675dd535058
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-20 13:09:35 +0700

    Update papa1.js

--- ori/papa1/papa1.js
+++ ori/papa1/papa1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI PAPA1
 // @namespace    http://tampermonkey.net/
-// @version      1.87
+// @version      1.88
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa1.js
@@ -141,7 +141,7 @@ var Backlist3 = "hasil";
 var Backlist4 = "room lomba freebet";
 var Backlist5 = "prediksi";
 var Backlist6 = "result";
-var Backlist7 = "juara";
+var Backlist7 = "totomacau";
 
 var jitter = 0
 var Cutter = 0
