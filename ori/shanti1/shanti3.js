cbf7455becf7d31e00b8ac3b84f6bad70871f8d6
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-20 13:07:18 +0700

    Update shanti3.js

--- ori/shanti1/shanti3.js
+++ ori/shanti1/shanti3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 1 SHANTI3
 // @namespace    http://tampermonkey.net/
-// @version      1.97
+// @version      1.98
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti3.js
@@ -142,7 +142,7 @@ var Backlist3 = "hasil";
 var Backlist4 = "room lomba freebet";
 var Backlist5 = "prediksi";
 var Backlist6 = "result";
-var Backlist7 = "juara";
+var Backlist7 = "totomacau";
 
 var jitter = 0
 var Cutter = 0
