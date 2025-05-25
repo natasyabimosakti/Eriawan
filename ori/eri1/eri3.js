72721e80a52714fea481dc6f8e20aac4d9ad505a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-25 19:17:25 +0700

    Update eri3.js

--- ori/eri1/eri3.js
+++ ori/eri1/eri3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI ERI3
 // @namespace    http://tampermonkey.net/
-// @version      2.90
+// @version      2.91
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri1/eri3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri1/eri3.js
@@ -70,7 +70,7 @@ var Comment18 = '';
 
 var refresh = 40;
 
-var admin = ["andika","anto","angelina","aura","anisa","aprilia","ayesha","alicia","ajosultan87","aqisya","azahra","aini",
+var adminList = ["andika","anto","angelina","aura","anisa","aprilia","ayesha","alicia","ajosultan87","aqisya","azahra","aini",
              "audi","andre","arem","alyn","alicia","asri","aldi","abys","angeline",
              "ban nee","bella","biru","bela","baran","bogard",
              "chandra","cengkiledu","chintya","clone","casaundra","chikaku","callista","ccwek","chow","chaow",
