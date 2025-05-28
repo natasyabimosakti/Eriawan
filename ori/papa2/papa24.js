dbcf65284a827d32153bac4f79f0a7255971cea0
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-28 17:45:27 +0700

    Update papa24.js

--- ori/papa2/papa24.js
+++ ori/papa2/papa24.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BO1 PAPA24
 // @namespace    http://tampermonkey.net/
-// @version      1.01
+// @version      1.02
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa2/papa24.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa2/papa24.js
@@ -95,7 +95,7 @@ var adminList = ["andika","anto","angelina","aura","anisa","aprilia","ayesha","a
              "tink","tesa","uuch","toto","utarii","tiar",
              "vonny",
              "wijaya","wawan",
-             "xabiru",
+             "xabiru","zeuz",
              "yoky"];
 
 var keyword = ["ROOM","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌","HONGKONG","SINGAPUR","nemo"]
