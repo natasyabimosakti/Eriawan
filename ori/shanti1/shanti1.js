01dc761ca81e110bc1fd76890add4b42b148544f
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-06-08 20:13:55 +0700

    Update shanti1.js

--- ori/shanti1/shanti1.js
+++ ori/shanti1/shanti1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 1 SHANTI1
 // @namespace    http://tampermonkey.net/
-// @version      4.77
+// @version      4.78
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti1.js
@@ -95,7 +95,7 @@ var adminList = ["angeline","ardiyanti","astrina","andinincy","asyati","alika","
              "toto","thonex",
              "vero",
              "wulan","wei","wawan","walid",
-             "xabiru",
+             "xabiru","yansah",
              "yoky"];
 
 var keyword = ["ROOM","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌","HONGKONG","SINGAPORE","ℝ𝕆𝕆𝕄","nemo"]
