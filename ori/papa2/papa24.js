9031841d939bb479ec54999a583b26996b49ee61
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-06-13 12:34:03 +0700

    Update papa24.js

--- ori/papa2/papa24.js
+++ ori/papa2/papa24.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BO1 PAPA24
 // @namespace    http://tampermonkey.net/
-// @version      1.03
+// @version      1.04
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa2/papa24.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa2/papa24.js
@@ -77,7 +77,7 @@ var refresh = 40;
 var adminList = ["andika","anto","angelina","aura","anisa","aprilia","ayesha","alicia","ajosultan87","aqisya","azahra","aini",
              "audi","andre","arem","alyn","alicia","asri","aldi","abys","angeline",
              "ban nee","bella","biru","bela","baran","bogard",
-             "chandra","cengkiledu","chintya","clone","casaundra","chikaku","callista","ccwek","chow","chaow",
+             "chandra","cengkiledu","chintya","clone","casaundra","chikaku","callista","ccwek","chow","chaow","christy",
              "dika","dewa","dea","damara",
              "ethes","enzo","eryna",
              "faiq","fania","ferlita",
