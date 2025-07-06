5b7f50a657ee8f345edda0b483f882cf12247816
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-06 08:31:12 +0700

    Update eri3.js

--- ori/eri1/eri3.js
+++ ori/eri1/eri3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI ERI3
 // @namespace    http://tampermonkey.net/
-// @version      3.00
+// @version      3.01
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri1/eri3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri1/eri3.js
@@ -63,7 +63,7 @@ var namagroup15 = 'Hoho';
 var Comment15 = 'INDRO787 : 38*17*42 #HOHOTOGEL';
 
 var namagroup16 = 'TOYIBSLOT';
-var Comment16 = '#TOYIBSLOT ( MANDRA16 ) : 38*17';
+var Comment16 = '#TOYIBSLOT (MANDRA16) = 38*17';
 
 var namagroup17 = 'MALAM';
 var Comment17 = 'MALAM FB3';
