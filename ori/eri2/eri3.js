beb66ba1b68f9594d2c0935e1d87b55f1f4307db
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-18 05:59:44 +0700

    Update eri3.js

--- ori/eri2/eri3.js
+++ ori/eri2/eri3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 2 ERI3
 // @namespace    http://tampermonkey.net/
-// @version      2.96
+// @version      2.97
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri3.js
