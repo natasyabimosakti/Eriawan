ba67f4165f79a41610b93bfa1f7dfbf332eef73e
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-24 00:19:34 +0700

    Update shanti4.js

--- ori/shanti1/shanti4.js
+++ ori/shanti1/shanti4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI 1 SHANTI4
 // @namespace    http://tampermonkey.net/
-// @version      2.24
+// @version      2.25
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/shanti1/shanti4.js
@@ -46,7 +46,7 @@ var namagroup9 = 'UPINSLOT';
 var Comment9 = '#UPINSLOT ( HOPENG596 ) 93*33*82 BETTING';
 
 var namagroup10 = 'KIKOTOTO';
-var Comment10 = '#KIKOTOTO (HOPENG596) = 62*85';
+var Comment10 = '#KIKOTOTO (HOPENG596) = 36*26';
 
 var namagroup11 = 'LING';
 var Comment11 = 'LING77 (NARITA74) 45*72';
@@ -75,13 +75,11 @@ var namagroup18 = 'Jawatengah';
 var Comment18 = ' GRUP B4  ';
 
 
-
-
 var SCRIPT_NAME = Comment18
 var refresh = 40;
 var URLADMIN = "https://raw.githubusercontent.com/natasyabimosakti/ADMIN/main/adminer2.json"
 var keyword = ["ROOM","RUM","𝑹𝑶𝑶𝑴","𝗥𝟬𝟬𝗠","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌","HONGKONG","SINGAPUR","SINGAPORE","nemo"]
-var Backlist =["pemenang lomba","rekap","natidulu","room lomba freebet","prediksi","result","juara lomba","r3k4p","r3kap","rek4p"]
+var Backlist =["pemenang lomba","rekap","natidulu","room lomba freebet","prediksi","result","juara lomba","r3k4p","TTM","r3kap","rek4p"]
 var isCommenting = false;
 var EXPIRATION_MS = 8 * 60 * 1000; // 5 minutes
 var now = Date.now();
@@ -317,7 +315,7 @@ async function manageGroups() {
 let sedangKlikTextbox = false;
 function CekBacklist(postinganBL) {
     for (const DataBacklist of Backlist) {
-        const kata = normalizeToBasicLatin(DataBacklist.toLowerCase())
+        const kata = DataBacklist.toLowerCase()
         if (postinganBL.toLowerCase().includes(kata)) {
             console.log(`❌ Diblok karena mengandung: "${kata}"`);
             return true;
@@ -329,7 +327,7 @@ function CekBacklist(postinganBL) {
 function CekKeyword(postingan) {
     console.log("🔍 CekKeyword untuk:", postingan);
     for (const DataKeyword of keyword) {
-        const kata = normalizeToBasicLatin(DataKeyword.toLowerCase())
+        const kata = DataKeyword.toLowerCase()
         if (postingan.toLowerCase().includes(kata)) {
             console.log(`✅ Keyword ditemukan: "${kata}"`);
             return true;
